b26cf0f92369d844e7e1c3bb3711efef77072dce
wuhan <1147306550@qq.com>
2021-03-28 20:30:07 +0800

    实现用户登录注册功能

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "backend/database"
+	"backend/thesisSearch"
 	"backend/user"
 	"github.com/gin-gonic/gin"
 )
@@ -10,9 +11,16 @@ import (
 func main() {
 
 	r := gin.Default()
+
+	// username=?&&password=?
 	r.GET("/user/login", user.UserLogin)
+
+	// username=?&&password=?
 	r.GET("/user/register", user.UserRegister)
-	r.GET("/search/list", user.GetThesisList)
+
+	// source=?&&keyword=?&&year=?
+	r.GET("/search/list", thesisSearch.GetThesisList)
+
 	//r.POST("/user", user.HandleList)
 	r.GET("/")
 	r.Run(":8080")
--- backend/user/user.go
+++ backend/user/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"backend/database"
 	_ "backend/database"
 	_ "database/sql"
 	"github.com/gin-gonic/gin"
@@ -19,17 +20,46 @@ type AnalyzedThesis struct {
 	Abstract string
 }
 
+// UserLogin 用户登录
 func UserLogin(c *gin.Context){
-	kw := c.Query("kw")
-	year := c.Query("year")
-	c.JSON(http.StatusOK, gin.H{"name":kw, "year":year})
+	username := c.Query("username")
+	password := c.Query("password")
+
+	ifExist, _ := database.DB.Raw("select * from user where username = ? and password = ?", username, password).Rows()
+	var i int
+	if ifExist.Next() {
+		i++
+	}
+
+	if (i == 1) {
+		c.JSON(http.StatusOK, gin.H{"ifCorrect":true})
+	}else {
+		c.JSON(http.StatusOK, gin.H{"ifCorrect":false})
+	}
+
 }
 
+// UserRegister 用户注册
 func UserRegister(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"name":"list"})
+	username := c.Query("username")
+	password := c.Query("password")
+
+	rows, _ := database.DB.Raw("select * from user where username = ? ", username).Rows()
+	var i int
+	if rows.Next() {
+		i++
+	}
+
+	if (i == 0 && len(username) <= 60 && len(password) >= 6) {
+		affected := database.DB.Exec("insert into user values(?, ?)", username, password).RowsAffected
+		if(affected == 1){
+			c.JSON(http.StatusOK, gin.H{"ifSucceed":true})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"ifSucceed":false})
+		}
+	} else {
+		c.JSON(http.StatusOK, gin.H{"ifSucceed":false})
+	}
 }
 
-func GetThesisList(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"name":"list"})
-}
 
